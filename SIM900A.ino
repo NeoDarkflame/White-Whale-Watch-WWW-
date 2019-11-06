4db73afe93e3777d47c9be30bc99e0ec4b9b086b
Bot-Bank100 <56309214+Bot-Bank100@users.noreply.github.com>
2019-11-06 17:49:26 +0700

    Working files with arduno

---
+++ SIM900A.ino
@@ -0,0 +1,53 @@
+#include <SoftwareSerial.h>
+SoftwareSerial SIM900A(10,11);
+void setup()
+{
+  SIM900A.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  Serial.println ("SIM900A Ready");
+  delay(100);
+  Serial.println ("Type s to send message or r to receive message");
+}
+void loop()
+{
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      SendMessage();
+      break;
+    case 'r':
+      RecieveMessage();
+      break;
+  }
+ if (SIM900A.available()>0)
+   Serial.write(SIM900A.read());
+}
+
+
+ void SendMessage()
+{
+  Serial.println ("Sending Message");
+  SIM900A.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);
+  Serial.println ("Set SMS Number");
+  SIM900A.println("AT+CMGS=\"+660917499494\"\r"); //Mobile phone number to send message
+  delay(1000);
+  Serial.println ("Set SMS Content");
+  SIM900A.println("Test");// Messsage content
+  delay(100);
+  Serial.println ("Finish");
+  SIM900A.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+  Serial.println ("Message has been sent");
+}
+
+
+ void RecieveMessage()
+{
+  Serial.println ("SIM900A Membaca SMS");
+  delay (1000);
+  SIM900A.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
+  delay(1000);
+  Serial.write ("Unread Message done");
+}
