e7f57762ad9b25ab4941ecf81e08dfc9f4e8ba09
Bot-Bank100 <56309214+Bot-Bank100@users.noreply.github.com>
2019-12-04 23:08:45 +0700

    Update Project_PSIT.ino

--- Project_PSIT.ino
+++ Project_PSIT.ino
@@ -1,23 +1,23 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
-SoftwareSerial SIM900A(10,11); //TX|RX
+SoftwareSerial SIM900A(10,11);// TX|RX
 int hour=12,minute=0,second=0;
 int check=0,recall=0;
 const float OffSet=0.483;
 float V,P;
-void setup() {
+void setup() {//Just a setting
   lcd.begin();
-  Serial.begin(9600);
-  SIM900A.begin(9600);
+  Serial.begin(9600);// Setting the baud rate of Serial Monitor (Arduino)
+  SIM900A.begin(9600);// Setting the baud rate of GSM Module
   lcd.backlight();
   Serial.println("SIM900A Ready");
-}//Just a setting
+}
 void loop() {
-  V=analogRead(0)*5.00/1024;//Receive values ​​from pressure sensors
-  P=(V-OffSet)*400;//Convert to kilopascal
+  V=analogRead(0)*5.00/1024;// Receive values from pressure sensors
+  P=(V-OffSet)*400;// Convert to kilopascal
   second++;
-  if (second>59){//Just a timer
+  if (second>59){// Just a timer
     minute++;
     second=0;
   }
@@ -28,7 +28,7 @@ void loop() {
   if (hour>23){
     hour=0;
   }
-  lcd.clear();//Display to LCD screen
+  lcd.clear();// Display to LCD screen
   lcd.print("TIME: ");
   lcd.print(hour);
   lcd.print(":");
@@ -40,12 +40,12 @@ void loop() {
   Serial.print(P,1);
   Serial.println(" KPa");
   delay(990);
-  if (P>20.0){//Sends a message when the P over 20 kilopascal
+  if (P>20.0){// Sends a message when the P over 20 kilopascal
     SendMessage();
     second++;
     check=1;
   }
-  while (check!=0){//Check if the child is still drowned
+  while (check!=0){// Check if the child is still drowned
     recall++;
     second++;
     Serial.println(recall);
@@ -60,16 +60,16 @@ void loop() {
 void SendMessage(){
   //Fuction used to send messages
   Serial.println("Sending Message");
-  SIM900A.println("AT+CMGF=1");
+  SIM900A.println("AT+CMGF=1");// Sets the GSM Module in Text Mode
   delay(200);
   Serial.println("Set SMS Number");
-  SIM900A.println("AT+CMGS=\"+660917499494\"\r");
+  SIM900A.println("AT+CMGS=\"+660917499494\"\r");// Mobile phone number to send message
   delay(200);
   Serial.println("Set SMS Content");
-  SIM900A.println("WHITEWHALEWATCH");
+  SIM900A.println("WHITEWHALEWATCH");// Messsage content
   delay(200);
   Serial.println("Finish");
-  SIM900A.println((char)26);//ASCII code of CTRL+Z
+  SIM900A.println((char)26);// ASCII code of CTRL+Z
   delay(400);
   Serial.println("Message has been sent");
 }
