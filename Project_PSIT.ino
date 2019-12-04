391c4804b3c8aab00d732e3553ece68a9cbddc52
Bot-Bank100 <56309214+Bot-Bank100@users.noreply.github.com>
2019-12-04 22:58:29 +0700

    Project_PSIT.ino
    โปรแกรมหลักในการทำงานของบอร์ดอาดูโน่

--- Project_PSIT.ino
+++ Project_PSIT.ino
@@ -1,19 +1,23 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27,16,2);
 SoftwareSerial SIM900A(10,11); //TX|RX
 int hour=12,minute=0,second=0;
-void setup() 
-{
+int check=0,recall=0;
+const float OffSet=0.483;
+float V,P;
+void setup() {
   lcd.begin();
   Serial.begin(9600);
   SIM900A.begin(9600);
   lcd.backlight();
   Serial.println("SIM900A Ready");
-}
+}//Just a setting
 void loop() {
+  V=analogRead(0)*5.00/1024;//Receive values ​​from pressure sensors
+  P=(V-OffSet)*400;//Convert to kilopascal
   second++;
-  if (second>59){
+  if (second>59){//Just a timer
     minute++;
     second=0;
   }
@@ -24,7 +28,7 @@ void loop() {
   if (hour>23){
     hour=0;
   }
-  lcd.clear();
+  lcd.clear();//Display to LCD screen
   lcd.print("TIME: ");
   lcd.print(hour);
   lcd.print(":");
@@ -33,14 +37,28 @@ void loop() {
   lcd.print(second);
   lcd.setCursor(1,1);
   lcd.print("WHITEWHALEWATCH");
+  Serial.print(P,1);
+  Serial.println(" KPa");
   delay(990);
-  if (Serial.read()=='s'){
+  if (P>20.0){//Sends a message when the P over 20 kilopascal
     SendMessage();
     second++;
+    check=1;
+  }
+  while (check!=0){//Check if the child is still drowned
+    recall++;
+    second++;
+    Serial.println(recall);
+    delay(990);
+    if (recall==10){
+      check=0;
+      recall=0;
+    }
   }
 }
 
-void SendMessage() {
+void SendMessage(){
+  //Fuction used to send messages
   Serial.println("Sending Message");
   SIM900A.println("AT+CMGF=1");
   delay(200);
