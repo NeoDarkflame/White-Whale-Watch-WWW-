7eef86cae69ca78fdb43902d4ccd51cc1f2bc5c3
Bot-Bank100 <56309214+Bot-Bank100@users.noreply.github.com>
2019-12-04 23:19:09 +0700

    File used to work with PROJECT_PSIT.ino (Pressure)
    โปรแกรมสำหรับวัดค่าแรงดันของน้ำ

--- Water_Pressure.ino
+++ Water_Pressure.ino
@@ -5,14 +5,14 @@ void setup() {
 }
 
 void loop() {
-  V = analogRead(0) * 5.00 / 1024;
-  P = (V-OffSet) * 400;
+  V = analogRead(0)*5.00/1024;// Receive values from pressure sensors
+  P = (V-OffSet)*400;// Convert to kilopascal
 
   Serial.print(V, 3);
-  Serial.println("V");
+  Serial.println("V");// Display V value to Serial Monitor
 
   Serial.print(P, 1);
-  Serial.println("KPa");
+  Serial.println("KPa");// Display P value to Serial Monitor
   Serial.println();
 
   delay(500);
